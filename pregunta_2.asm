c66595fc49375e3fe909f8150521ce1f73a159ca
Daniel Alejandro Morales Miguel <daniel@devwith.us>
2014-07-07 23:58:38 -0400

    report

--- pregunta_2.asm
+++ pregunta_2.asm
@@ -8,15 +8,15 @@ main:
 	la $a0, msg1
 	syscall
 
-	#li.s 		$f0, 2.0
-	#li.s 		$f1, 1.0
-	#div.s 		$f12, $f1, $f0
+	#li.s $f0, 2.0
+	#li.s $f1, 1.0
+	#div.s $f12, $f1, $f0
 
 	#Initialise:
-	li.s 	$f0, 0.0 #f0
-	li.s 	$f1, 1.0 #f1
-	li.s    $f2, 1.0 #f2
-	li.s    $f5, 1.0
+	li.s $f0, 0.0 #f0
+	li.s $f1, 1.0 #f1
+	li.s $f2, 1.0 #f2
+	li.s $f5, 1.0
 	
 	#Request user input:
 	li $v0, 5
@@ -30,10 +30,10 @@ fib:
 	j exit
 
 continue:
-	mov.s $f4, $f1  		;#$f4 temp
+	mov.s $f4, $f1
 	add.s $f1, $f1, $f0
 	div.s $f0, $f5, $f1
-	add.s $f2, $f2, $f0  			;#f2 stores div
+	add.s $f2, $f2, $f0
 	mov.s $f0, $f4
 	sub $s3, $s3, 1
 	j fib
